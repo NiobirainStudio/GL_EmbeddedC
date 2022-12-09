4e9dd98555294d40380221ccb1987725d1c9bef8
NidvenQ <niobirain@gmail.com>
2022-12-09 19:42:32 +0200

    Adding the first task:

---
+++ README.md
@@ -0,0 +1,5 @@
+# GL_EmbeddedC
+A set of tasks for C/Embedded
+
+Task_1 execution result:
+![result_1](task_1/result.png)
---
+++ task_1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+   char* group_num   = "group-4";
+   char* full_name   = "Horhul Daniel";
+   char* email       = "daniel.horhul@gmail.com";
+   char* repo_link   = "https://github.com/NiobirainStudio/GL_EmbeddedC";
+
+   printf("Group Number:  %s\nFull Name:     %s\nEmail:         %s\nRepo Link:     %s\n",
+      group_num,
+      full_name,
+      email,
+      repo_link
+   );
+
+   return 0;
+}
