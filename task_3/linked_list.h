f0b172a2d03c3118249a2dcea30efa27d37bfa98
DreaDver <niobirain@gmail.com>
2022-12-26 00:42:06 +0200

    Migration to win

--- task_3/linked_list.h
+++ task_3/linked_list.h
@@ -1,6 +1,87 @@
 #pragma once
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void Hello() {
-    printf("Hi\n");
+
+/*============================================================//
+    LINKED LIST STRUCT PAGE BEGIN
+//============================================================*/
+typedef struct SizedArray {
+    void* data;
+    unsigned int length;
+} SizedArray;
+
+typedef struct LinkedListNode {
+    struct LinkedListNode* next;   // Ptr to the next node
+    void* data;             // Ptr to node data
+} LinkedListNode;
+
+typedef struct LinkedList {
+    LinkedListNode* first;   // Ptr to the first node
+    unsigned int length;     // Length of the list
+    unsigned int type_size;  // Size of the node type
+} LinkedList;
+/*============================================================//
+    LINKED LIST STRUCTS END
+//============================================================*/
+
+
+
+
+/*============================================================//
+    LINKED LIST FUNCTION PAGE BEGIN
+//============================================================*/
+LinkedList* linked_list_create(unsigned int type_size) {
+    LinkedList* list = (LinkedList*)malloc(sizeof(LinkedListNode));
+    list->length     = 0;
+    list->type_size  = type_size;
+    list->first      = NULL;
+    return list;
 }
+
+void linked_list_free(LinkedList* target) {
+    LinkedListNode* current = target->first;
+    LinkedListNode* next = NULL;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    free(target);
+}
+
+void linked_list_push(LinkedList* target, void* data) {
+    LinkedListNode* prev_start = target->first;
+    LinkedListNode* new_start = (LinkedListNode*)malloc(target->type_size);
+
+    new_start->data = data;
+    new_start->next = prev_start;
+
+    target->first = new_start;
+    target->length++;
+}
+
+SizedArray* linked_list_to_arr(LinkedList* target) {
+    
+    SizedArray* s_arr = (SizedArray*)malloc(sizeof(SizedArray));
+    s_arr->data = malloc(target->length * target->type_size);
+    s_arr->length = target->length;
+
+    LinkedListNode* node = target->first;
+
+
+    for (unsigned int i = 0; i < target->length; i++)
+    {
+        memcpy(s_arr->data + i*target->type_size, &node->data, target->type_size);
+        node = node->next;
+    }
+
+    return s_arr;
+}
+/*============================================================//
+    LINKED LIST FUNCTION PAGE END
+//============================================================*/
--- task_3/main.c
+++ task_3/main.c
@@ -1,9 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv)
+#include "linked_list.h"
+
+
+// Struct of interest
+typedef struct
+{//Type           Name           Size  Padding  Struct size
+   //char*          title;         // 8  0        8
+   //float          price;         // 4  4        16
+   unsigned short page_num;      // 2  2        16
+   //unsigned short release_year;  // 2  0        16
+   //char*          lang;          // 8  0        24
+   //float          weight;        // 4  4        32
+} Book;
+
+
+// Printing section
+void linked_list_print_by_arr(LinkedList* target) {
+   SizedArray* arr = linked_list_to_arr(target);
+
+   for (unsigned int i = 0; i < arr->length; i++) {
+      printf("item: %d\n", ((int*)arr->data)[i]);
+   }
+
+   free(arr->data);
+   free(arr);
+}
+
+void linked_list_print_by_link(LinkedList* target) {
+   LinkedListNode* current = target->first;
+
+   while (current != NULL)
+   {
+      Book* item = (Book*)current->data;
+      
+      printf("Item: %d\n", item->page_num);
+
+      current = current->next;
+   }
+}
+
+
+int main()
 {
-   char * buffer = malloc(1024);
-   sprintf(buffer, "%d", argc);
-   printf("%s",buffer);
+   LinkedList* l_list = linked_list_create(sizeof(Book));
+
+   Book* b1 = malloc(sizeof(Book));
+
+   //b1->title = "Ass";
+   //b1->price = 4.4f;
+   b1->page_num = 5;
+   //b1->release_year = 1999;
+   //b1->lang = "en";
+   //b1->weight = 4.1f;
+
+   linked_list_push(l_list, b1);
+
+   //linked_list_print_by_link(l_list);
+
+   linked_list_free(l_list);
+
+   return 0;
 }
