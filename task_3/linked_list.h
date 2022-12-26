e6a89989d28d1af89b7457b9dc4829655b32947b
NiobirainStudio <niobirain@gmail.com>
2022-12-26 04:27:41 +0200

    Task_3 v2

--- task_3/linked_list.h
+++ task_3/linked_list.h
@@ -7,23 +7,26 @@
 /*============================================================//
     LINKED LIST STRUCT PAGE BEGIN
 //============================================================*/
+// An array of undefined types with size
 typedef struct SizedArray {
     void* data;
     unsigned int length;
 } SizedArray;
 
+// A single node of a list
 typedef struct LinkedListNode {
-    struct LinkedListNode* next;   // Ptr to the next node
-    void* data;             // Ptr to node data
+    struct LinkedListNode* next;    // Ptr to the next node
+    void* data;                     // Ptr to node data
 } LinkedListNode;
 
+// List head structure
 typedef struct LinkedList {
     LinkedListNode* first;   // Ptr to the first node
     unsigned int length;     // Length of the list
     unsigned int type_size;  // Size of the node type
 } LinkedList;
 /*============================================================//
-    LINKED LIST STRUCTS END
+    LINKED LIST STRUCT PAGE END
 //============================================================*/
 
 
@@ -32,56 +35,95 @@ typedef struct LinkedList {
 /*============================================================//
     LINKED LIST FUNCTION PAGE BEGIN
 //============================================================*/
+// Initializes the list
 LinkedList* linked_list_create(unsigned int type_size) {
-    LinkedList* list = (LinkedList*)malloc(sizeof(LinkedListNode));
-    list->length     = 0;
-    list->type_size  = type_size;
-    list->first      = NULL;
+    LinkedList* list = malloc(sizeof(LinkedList));
+    
+    if (list != NULL) {
+        list->length     = 0;
+        list->type_size  = type_size;
+        list->first      = NULL;
+    }
+
     return list;
 }
 
+// Destroy the entire list
 void linked_list_free(LinkedList* target) {
-    LinkedListNode* current = target->first;
-    LinkedListNode* next = NULL;
+    LinkedListNode* head = target->first;
 
-    while (current != NULL)
+    while (head != NULL)
     {
-        next = current->next;
-        free(current);
-        current = next;
+        LinkedListNode* node = head;
+        head = head->next;
+        free(node->data);
+        free(node);
     }
 
     free(target);
 }
 
+// Add an element to the head of the list
 void linked_list_push(LinkedList* target, void* data) {
-    LinkedListNode* prev_start = target->first;
-    LinkedListNode* new_start = (LinkedListNode*)malloc(target->type_size);
+    LinkedListNode* new_head = malloc(target->type_size);
 
-    new_start->data = data;
-    new_start->next = prev_start;
+    // Memory check
+    if (new_head == NULL)
+        return;
+    
+    // Enough memory
+    new_head->data = data;
+    new_head->next = target->first;
 
-    target->first = new_start;
+    target->first = new_head;
     target->length++;
 }
 
-SizedArray* linked_list_to_arr(LinkedList* target) {
-    
-    SizedArray* s_arr = (SizedArray*)malloc(sizeof(SizedArray));
-    s_arr->data = malloc(target->length * target->type_size);
-    s_arr->length = target->length;
+// Add an element to the tail of the list
+void linked_list_append(LinkedList* target, void* data) {
+    LinkedListNode* new_head = malloc(target->type_size);
 
-    LinkedListNode* node = target->first;
+    // Memory check
+    if (new_head == NULL)
+        return;
 
+    // Enough memory
+    new_head->data = data;
+    new_head->next = NULL;
 
-    for (unsigned int i = 0; i < target->length; i++)
-    {
-        memcpy(s_arr->data + i*target->type_size, &node->data, target->type_size);
-        node = node->next;
+    // If the node is first
+    if (target->first == NULL) {
+        target->first = new_head;
+        
+    } else {
+        LinkedListNode* navi = target->first;
+        while (navi->next != NULL)
+            navi = navi->next;
+        
+        navi->next = new_head;
     }
 
-    return s_arr;
+    target->length++;
 }
+
+// Convert list to SizedArray
+// SizedArray* linked_list_to_arr(LinkedList* target) {
+    
+//     SizedArray* s_arr = (SizedArray*)malloc(sizeof(SizedArray));
+//     s_arr->data = malloc(target->length * target->type_size);
+//     s_arr->length = target->length;
+
+//     LinkedListNode* node = target->first;
+
+
+//     for (unsigned int i = 0; i < target->length; i++)
+//     {
+//         memcpy(s_arr->data + i*target->type_size, &node->data, target->type_size);
+//         node = node->next;
+//     }
+
+//     return s_arr;
+// }
 /*============================================================//
     LINKED LIST FUNCTION PAGE END
 //============================================================*/
